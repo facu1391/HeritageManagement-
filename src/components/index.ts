11f0afffa5bfd1451f9740ab1519de9656a939ae
Facundo <facurael@gmail.com>
2024-12-09 18:22:09 -0300

    feat(navbar): add responsive search bar and toggleable profile dropdown menu

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
+
 //export { default as Footer } from "./Layouts/Footer/Footer";
-export { default as Sidebar } from "./Layouts/Sidebar/Sidebar"
+export { default as Sidebar } from "./Layouts/Sidebar/Sidebar";
+export { default as Navbar } from "./Layouts/NavBar/NavBar";
