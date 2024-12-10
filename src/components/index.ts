184b791f5eaf76ca5cc6b796e77d734f13850bcb
Facundo <facurael@gmail.com>
2024-12-10 10:26:00 -0300

    fix:logo

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 //export { default as Footer } from "./Layouts/Footer/Footer";
 export { default as Sidebar } from "./Layouts/Sidebar/Sidebar";
-export { default as NavBar } from "./Layouts/NavBar/Navbar";
+
