6b359c4801412f3a2a98927275eaf2cfe33f1f79
Facundo <facurael@gmail.com>
2024-12-10 10:25:05 -0300

    fix:navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Sidebar } from "@/components";
+
+
 
 export default function Home() {
   return (
     <div>
-      <Sidebar />
+    
+     
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 //export { default as Footer } from "./Layouts/Footer/Footer";
-export { default as Sidebar } from "./Layouts/Sidebar/Sidebar"
+export { default as Sidebar } from "./Layouts/Sidebar/Sidebar";
+export { default as NavBar } from "./Layouts/NavBar/Navbar";
