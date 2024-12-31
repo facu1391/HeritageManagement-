8d68c62effc01b6765c5732776abd9673cfbcc93
Facundo <facurael@gmail.com>
2024-12-31 16:27:56 -0300

    fix: dark-bg

--- src/components/Layouts/NavBar/NavBar.tsx
+++ src/components/Layouts/NavBar/NavBar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-gray-800 dark:bg-gray-900">
+    <nav className="bg-gray-800 dark:bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           {/* Buscador centrado */}
