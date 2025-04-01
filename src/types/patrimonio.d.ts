18605a4f716c0dacfe4baf1c6a0fabada0f2c887
Facundo <facurael@gmail.com>
2025-04-01 12:33:58 -0300

    feat:mobilarioService

---
+++ src/types/patrimonio.d.ts
@@ -0,0 +1,19 @@
+
+export interface PatrimonioData {
+    id: string;
+    ubicacion_id: number;
+    descripcion: string;
+    resolucion_numero: string;
+    resolucion_tipo: string;
+    fecha_resolucion: string;
+    estado_conservacion: string;
+    no_dado: boolean;
+    reparacion: boolean;
+    para_baja: boolean;
+    faltante: boolean;
+    sobrante: boolean;
+    etiqueta: boolean;
+    comentarios: string;
+    foto_url: string;
+  }
+  
