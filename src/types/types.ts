3c814356fec11e59d009d1118b5e0b875f7c1520
Facundo <facurael@gmail.com>
2025-04-27 20:37:46 -0300

    type.ts: patrimoniodata

--- src/types/types.ts
+++ src/types/types.ts
@@ -72,3 +72,23 @@ export interface Mobiliario {
   fecha_creacion: string;
   fecha_actualizacion: string;
 }
+
+// --- 🔥 FALTABA AGREGAR ESTA INTERFAZ 🔥 ---
+// Este es el shape que se manda cuando creas un patrimonio
+export interface PatrimonioData {
+  id: string;
+  ubicacion_id: number;
+  descripcion: string;
+  resolucion_numero: string;
+  resolucion_tipo: string;
+  fecha_resolucion: string;
+  estado_conservacion: string;
+  no_dado: boolean;
+  reparacion: boolean;
+  para_baja: boolean;
+  faltante: boolean;
+  sobrante: boolean;
+  etiqueta: boolean;
+  comentarios: string;
+  foto_url: string;
+}
